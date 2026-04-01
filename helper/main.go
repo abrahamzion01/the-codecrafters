051141b7b64b06c8f9f6154368d1778d806d7202
jobiabo <jobiabo@noreply.acad.learn2earn.ng>
2026-04-01 13:15:37 +0000

    Update helper/main.go

--- helper/main.go
+++ helper/main.go
@@ -1,23 +1,23 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"os"
+// )
 
-func main() {
+// func main() {
 
-	if len(os.Args) != 3 {
-		fmt.Println("go run . input.txt output.txt")
-	} else {
-		fmt.Println("Converted successfully")
-	}
+// 	if len(os.Args) != 3 {
+// 		fmt.Println("go run . input.txt output.txt")
+// 	} else {
+// 		fmt.Println("Converted successfully")
+// 	}
 
-	inputfile := os.Args[1]
-	outputfile := os.Args[2]
+// 	inputfile := os.Args[1]
+// 	outputfile := os.Args[2]
 
-	content := readFile(inputfile)
-	content = fixAToAn(content)
+// 	content := readFile(inputfile)
+// 	content = fixAToAn(content)
 
-	writeFile(outputfile, content)
-}
+// 	writeFile(outputfile, content)
+// }
